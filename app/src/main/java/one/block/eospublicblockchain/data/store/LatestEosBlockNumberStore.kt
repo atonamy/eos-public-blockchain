880c824dd9c66ccb3e2d0014373cdfaa9e335475
atonamy <fruktoed@gmail.com>
2020-01-31 12:43:26 +0800

    forgot to remove store

--- app/src/main/java/one/block/eospublicblockchain/data/store/LatestEosBlockNumberStore.kt
+++
@@ -1,6 +0,0 @@
-package one.block.eospublicblockchain.data.store
-
-import com.dropbox.android.external.store4.Store
-import java.math.BigInteger
-
-class LatestEosBlockNumberStore(val value: Store<Unit, BigInteger>)
