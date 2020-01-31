513f30bf1a89f4e647643a0f2a34ba75ddffac42
atonamy <fruktoed@gmail.com>
2020-01-31 08:50:57 +0800

    viewModelScope trigger ANR

--- app/src/main/java/one/block/eospublicblockchain/view_model/EosBlocksViewModel.kt
+++ app/src/main/java/one/block/eospublicblockchain/view_model/EosBlocksViewModel.kt
@@ -1,6 +1,5 @@
 package one.block.eospublicblockchain.view_model
 
-import androidx.lifecycle.viewModelScope
 import com.airbnb.mvrx.*
 import com.dropbox.android.external.store4.StoreRequest
 import kotlinx.coroutines.delay
@@ -22,7 +21,7 @@ class EosBlocksViewModel(
         initKoinScope<EosBlocksViewModel>()
         asyncSubscribe(EosBlocksState::latestBlockNumber) {
             withState { state ->
-                viewModelScope.launch {
+                scope.launch {
                     for(i in 0 until state.totalBlocks) {
                         requestSpecificBlock(it.minus(BigInteger.valueOf(i.toLong())))
                         delay(250) //eos rpc throw error if load simultaneously
--- app/src/main/java/one/block/eospublicblockchain/view_model/MvRxViewModel.kt
+++ app/src/main/java/one/block/eospublicblockchain/view_model/MvRxViewModel.kt
@@ -1,6 +1,5 @@
 package one.block.eospublicblockchain.view_model
 
-import androidx.lifecycle.viewModelScope
 import com.airbnb.mvrx.*
 import com.dropbox.android.external.store4.StoreResponse
 import kotlinx.coroutines.*
@@ -14,8 +13,10 @@ import org.koin.core.scope.Scope
 abstract class MvRxViewModel<S : MvRxState>(initialState: S, private val koin: Koin) :
     BaseMvRxViewModel<S>(initialState, debugMode = BuildConfig.DEBUG) {
 
+    protected val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
     protected lateinit var koinScope: Scope
-    
+
+
     @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
     protected inline fun <reified T> initKoinScope() {
         koinScope = koin.getOrCreateScope("ViewModel", named<T>())
@@ -24,7 +25,7 @@ abstract class MvRxViewModel<S : MvRxState>(initialState: S, private val koin: K
     @Suppress("NON_PUBLIC_CALL_FROM_PUBLIC_INLINE")
     protected inline fun <T> Flow<StoreResponse<T>>.execute(crossinline stateReducer: S.(Async<T>) -> S) {
         var job: Job? = null
-        job = viewModelScope.launch {
+        job = scope.launch {
             try {
                 setState {
                     stateReducer(this, Loading())
@@ -66,6 +67,7 @@ abstract class MvRxViewModel<S : MvRxState>(initialState: S, private val koin: K
 
     override fun onCleared() {
         super.onCleared()
+        scope.cancel()
         if(::koinScope.isInitialized)
             koinScope.close()
     }
